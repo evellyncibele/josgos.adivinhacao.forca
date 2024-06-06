6d2440da64bf939dcf8b51321c8fe93fd29db0f8
evellyn cibele <128157952+evellyncibele@users.noreply.github.com>
2024-06-06 08:08:12 -0300

    Add files via upload

--- forca.py
+++ forca.py
@@ -11,6 +11,13 @@ def jogar_forca():
 
     #Enquanto o jogador não se "enforcar" E não acertar a palavra, faça algo
     while(not enforcou and not acertou):
+        chute = input("Qual a letra")
+
+        index = 0
+        for letra in palavraSecreta:
+            if(chute == letra):
+                print("Encontrei a letra {} na posição {}".format(letra, index))
+                index = index + 1
         print("Jogando....")
 
 
